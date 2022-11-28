75d3b7ce2c736482ac3f22ea58abbbda121ad749
Ryan Ally <xRyanAlly@gmail.com>
2022-11-28 17:27:26 -0500

    modified cardlist

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,9 +2,6 @@ import React from "react";
 import Card from "./Card";
 
 const CardList = ({ robots }) => {
-	if (true) {
-		throw new Error("NOOOOOOOOOOOOO");
-	}
 	const cardArray = robots.map((robot, i) => {
 		return <Card key={i} id={robot.id} name={robot.name} email={robot.email} />;
 	});
